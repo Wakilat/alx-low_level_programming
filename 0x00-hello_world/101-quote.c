c964d6b9fc2238105bcdb0e6fe59f474c5c9572c
Wakilat <wakilatadeola@gmail.com>
2022-03-10 09:33:50 -0800

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Print a statement followed by a new line, to the standard error
+ * main - Print a sentence followed by a new line, to the standard error
  *
  * Return: returns (1) after printing
  */
 
 int main(void)
 {
-	fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-       return (1);
-}       
+	wite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		return(1);
