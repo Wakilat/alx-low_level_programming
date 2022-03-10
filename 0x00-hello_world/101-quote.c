f676740618a2130444adcd46d7d3e09db667479c
Wakilat <wakilatadeola@gmail.com>
2022-03-10 10:05:52 -0800

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - Print a sentence followed by a new line, to the standard error
+ * main - Print a sentence  followed by a new line, to the standard error
  *
- * Return: returns (1) after printing
+ * Return: return (1) after printing
  */
 
 int main(void)
 {
-	write (1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		return(1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
