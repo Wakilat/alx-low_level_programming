5dc317028305e17c61321b9180c686d929958351
Wakilat <wakilatadeola@gmail.com>
2022-03-11 07:16:46 -0800

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,27 +8,35 @@
 
 int main(void)
 {
-	int huns;
-	int tens;
-	int ones;
+	int huns = 48;
+	int tens = 48;
+	int ones = 48;
 
-	for (huns = 0; huns <= 9; huns++)
+	while (tens < 58)
 	{
-		for (tens = huns + 1; tens <= 9; tens++)
+		huns = 48;
+		while (huns < 58)
 		{
-			for (ones = tens + 1; ones <= 9; ones++)
+			ones = 48;
+			while (ones < 58)
 			{
-				putchar(huns + '0');
-				putchar(tens + '0');
-				putchar(ones + '0');
-
-				if (huns < 8)
+				if (tens != huns && tens != ones && huns != ones && tens < huns && huns < ones)
 				{
+					putchar(tens);
+					putchar(huns);
+					putchar(ones);
+					if (huns == 56 && tens == 55 && ones == 57)
+					{
+						break;
+					}
 					putchar(',');
 					putchar(' ');
 				}
+				ones++;
 			}
+			huns++;
 		}
+		tens++;
 	}
 	putchar('\n');
 	return (0);
