998fe306cb16acfa4995893a84ab19750dcab620
Wakilat <wakilatadeola@gmail.com>
2022-03-11 06:48:22 -0800

    11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Lanzador
+ *
+ * Return: Always return (0)
+ */
+
+int main(void)
+{
+	int huns;
+	int tens;
+	int ones;
+
+	for (huns = 0; huns <= 9; huns++)
+	{
+		for (tens = hun + 1; tens <= 9; tens++)
+		{
+			 (ones = tens + 1; ones <= 9; ones++)
+			 {
+				 putchar(huns + '0');
+				 putchar(tens + '0');
+				 putchar(ones + '0');
+
+				 if (huns < 8)
+				 {
+					 putchar(',');
+					 putchar(' ');
+				 }
+			 }
+		}
+	}
+	putchar('\n');
+	return (0);
+}
