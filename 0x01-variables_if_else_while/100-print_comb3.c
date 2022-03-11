7e2f0b1d00db9f81ad0c1563b41ba501f759511a
Wakilat <wakilatadeola@gmail.com>
2022-03-11 05:39:27 -0800

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,14 @@ int main(void)
 	int tens;
 	int ones;
 
-	for (tens = '0'; tens <= '9'; tens++)
+	for (tens = 0; tens <= 9; tens++)
 	{
-		for (ones = tens + '1'; ones <= '9'; ones++)
+		for (ones = tens + 0; ones <= 9; ones++)
 		{
-			putchar(tens);
-			putchar(ones);
+			putchar(tens +'0');
+			putchar(ones + '0');
 
-			if (tens < '8')
+			if (tens < 8)
 			{
 				putchar(',');
 				putchar(' ');
