9e724d2dfdfc005acd9e3fd7df0f6fc0fb9cf151
Wakilat <wakilatadeola@gmail.com>
2022-03-11 06:57:03 -0800

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,17 +17,17 @@ int main(void)
 		for (tens = huns + 1; tens <= 9; tens++)
 		{
 			for (ones = tens + 1; ones <= 9; ones++)
-			 {
-				 putchar(huns + '0');
-				 putchar(tens + '0');
-				 putchar(ones + '0');
+			{
+				putchar(huns + '0');
+				putchar(tens + '0');
+				putchar(ones + '0');
 
-				 if (huns < 8)
-				 {
-					 putchar(',');
-					 putchar(' ');
-				 }
-			 }
+				if (huns < 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
