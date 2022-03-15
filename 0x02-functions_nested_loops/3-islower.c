092a272967f924d61d9452086b02113aadc3dbe0
Wakilat <wakilatadeola@gmail.com>
2022-03-15 10:48:44 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - Shows 1 if the input is a lowercase character. Another cases, shows 0
+ * _islower - checks for lowercase character
  * @c: The character in ASCII code
  * Return: 1 for lowercase character. 0 for the rest.
  */
