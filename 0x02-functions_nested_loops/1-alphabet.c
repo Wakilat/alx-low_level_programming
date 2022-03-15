7165d99b7ca25b2374b9767e5493929542d68f0f
Wakilat <wakilatadeola@gmail.com>
2022-03-15 08:00:40 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: returns (0)
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
