4f2100219b005fc3fbff1b743cc9d91bb75263b7
Wakilat <wakilatadeola@gmail.com>
2022-03-15 11:39:04 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - checks number
+ * @j: input number as an integer
+ * Return: last digit
+ */
+
+int print_last_digit(int j)
+{
+	int i;
+
+	i = j % 10;
+
+	if (i < 0)
+	{
+		i *= -1;
+	}
+	_putchar(i + '0');
+	return (i);
+}
