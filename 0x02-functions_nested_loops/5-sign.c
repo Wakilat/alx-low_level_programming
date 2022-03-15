e26192b00f7313735ac85e2b3156d9f5eae52f3e
Wakilat <wakilatadeola@gmail.com>
2022-03-15 11:08:53 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet - llamar la funcion desde el main
+ * @n: The input number as an integer.
+ * Return: 1 is greater than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+	_putchar('\n');
+}
