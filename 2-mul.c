dbadf8810be07863fb12602c08aa0500c3fd18ca
Wakilat <wakilatadeola@gmail.com>
2022-03-17 04:06:21 -0700

    3

---
+++ 2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: mul
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
