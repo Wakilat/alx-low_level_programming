ef4f1291b2aef60a1d143f56608f6d1a40f4c4c5
Wakilat <wakilatadeola@gmail.com>
2022-03-24 13:27:18 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Add one line to this code, so that the program prints a[2] = 98
- * Return: return (0)
- */
-
 int main(void)
 {
 	int n;
@@ -15,6 +10,11 @@ int main(void)
 	p = &n;
 	/*
 	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 *   - you are not allowed to modify p
+	 *   - only one statement
+	 *   - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
