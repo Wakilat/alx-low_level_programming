9578a9994cb5e9fd87f4c51fca9f3dfaa6efa73e
Wakilat <wakilatadeola@gmail.com>
2022-03-30 16:04:57 -0700

    ok

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,63 +7,44 @@
  * Return: 1 if the strings can be considered as identical, and 0
  */
 
-int bandersnatch(char *s1, char *s2);
-char *move(char *s2);
+int recursive_check(char *s1, char *s2, int wildC);
 
 int wildcmp(char *s1, char *s2)
 {
-	int sum = 0;
+	return (recursive_check(s1, s2, 0));
+}
 
-	if (*s1 == '\0' && *s2 == '*' && !*move(s2))
+/**
+ * recursive_check - checks for wild cards recursively
+ * @s1: string 1 to check (no wild cards)
+ * @s2: string to to check (can contain wildcards)
+ * @wildC: counter to see if wildcard is present
+ * Return: 1 if identical, 0 for not, -1 for error
+ */
+
+int recursive_check(char *s1, char *s2, int wildC)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
 		return (1);
-	if (*s1 == *s2)
-	{
-		if (*s1 == '\0')
-			return (1);
-		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	if (*s1 == '\0' || *s2 == '\0')
-		return (0);
 	if (*s2 == '*')
+		return (recursive_check(s1, s2 + 1, ++wildC));
+	else if (*s1 != *s2 && wildC > 0) /* skip to next rel char */
 	{
-		s2 = move(s2);
-		if (*s2 == '\0')
-			return (1);
-		if (*s1 == *s2)
-			sum += wildcmp(s1 + 1, s2 + 1);
-		sum += bandersnatch(s1 + 1, s2);
-		return (!!sum);
+		if (!*s1)
+			return (0);
+		return (recursive_check(s1 + 1, s2, wildC));
 	}
-	return (0);
-}
-
-/**
- * bandersnatch - checks recursively for all the paths when the
- * characters are equal
- * @s1: first string
- * @s2: second string
- * Return: return value of wildcmp() or of itself
- */
-
-int bandersnatch(char *s1, char *s2)
-{
-	if (*s1 == '\0')
+	else if (*s1 == *s2 && wildC == 0) /* char match, next char for each str */
+		return (recursive_check(s1 + 1, s2 + 1, 0));
+	else if (*s1 == *s2 && wildC > 0)
+	{
+		if (!recursive_check(s1 + 1, s2 + 1, 0))
+			return (recursive_check(s1 + 1, s2, wildC));
+		return (recursive_check(s1 + 1, s2 + 1, wildC));
+	}
+	else if (*s1 != *s2 && wildC == 0) /* non matching and no wildC */
 		return (0);
-	if (*s1 == *s2)
-		return (wildcmp(s1, s2));
-	return (bandersnatch(s1 + 1, s2));
-}
-
-/**
- * *move - moves the current char past the *
- * @s2: string to iterate over
- * Return: the address of the character after the *
- */
-
-char *move(char *s2)
-{
-	if (*s2 == '*')
-		return (move(s2 + 1));
-	else
-		return (s2);
+	return (-1); /* something unexpected occured, return -1 for error */
 }
