0eba9f3e370c245202c2ca70c6d794dfe1c2bf8a
Wakilat <wakilatadeola@gmail.com>
2022-04-01 14:48:47 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main -  writes its name
- * @argc: arguement count
- * @argv: array of pointers to arguement strings
- * Return: 0
+ * main - entry point for program
+ * @argc: count of args present
+ * @argv: array of char * pointing to args
+ * Return: always 0 (success)
  */
 
 int main(int argc, char *argv[])
 {
-	if (argc == 1)
-	{
 	printf("%s\n", argv[0]);
 	return (0);
-	}
 }
