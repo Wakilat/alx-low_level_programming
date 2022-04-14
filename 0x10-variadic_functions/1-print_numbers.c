1de2035519e7cb736b5415412d4b0bcdfc29bfc5
Wakilat <wakilatadeola@gmail.com>
2022-04-14 04:32:36 -0700

    1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,27 +1,42 @@
-#include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
 #include <unistd.h>
+void print_number(int n);
 
 /**
- * print_numbers - prints numbers
- * @separator: string to be printed between numbers
- * @n: number of integers passed to the function
- * Return: no return
+ * print_numbers - prints out numbers from variadic function
+ * @seperator: pointer to seperation string
+ * @n: number of numbers to print
+ * @...: numbers to print
+ * Return: always void
  */
 
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_numbers(const char *seperator, const unsigned int n, ...)
 {
-	va_list valist;
+	va_list nums;
 	unsigned int i;
+	int cur_num;
 
-	va_start(valist, n);
+	va_start(nums, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(valist, int));
-		if (separator && i < n - 1)
-			printf("%s", separator);
+		cur_num = va_arg(nums, int);
+		print_number(cur_num);
+		if (seperator == NULL || *seperator == '\0')
+			continue;
+		if (i != n - 1)
+			printf("%s", seperator);
 	}
-	printf("\n");
-	va_end(valist);
+	putchar('\n');
+	va_end(nums);
+}
+/**
+ * print_number - print number n with putchar
+ * @n: number to print
+ * Return: always void
+ */
+void print_number(int n)
+{
+	printf("%d", n);
 }
