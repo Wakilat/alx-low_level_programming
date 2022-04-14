9009439270f34857c8d57060a551f6e1452d9bab
Wakilat <wakilatadeola@gmail.com>
2022-04-14 04:14:31 -0700

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,11 +13,10 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-	int sum;
+	int sum = 0;
 
 	if (n == 0)
 		return (0);
-	sum = 0;
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
