585400b0017d9ee2f9078cfa205c3d48deaa7275
Wakilat <wakilatadeola@gmail.com>
2022-04-14 04:51:42 -0700

    2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,14 +2,12 @@
 #include "variadic_functions.h"
 
 /**
- * print_strings - prints all strings given, variadic
- * @seperator: seperator to print between strings
- * @n: number of strings
- * @...: strings to print
- * Return: always void
+ * print_strings - printsstrings followed by a new line
+ * @separator: seperators
+ * @n: number of arguements
  */
 
-void print_strings(const char *seperator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list strings;
 	unsigned int i;
