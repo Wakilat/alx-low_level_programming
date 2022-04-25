f66b18b8bec52dfbdbc8cd8720c384909d11ce58
Wakilat <wakilatadeola@gmail.com>
2022-04-25 12:03:35 -0700

    Linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,34 +1,37 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
 /**
- * delete_nodeint_at_index - deletes the node at index 
- * @head: head of the list
- * @index: emplacement of the node to delet
- * Return: pointr to head of list
+ * delete_nodeint_at_index - deletes an element of linked list at index
+ * @head: head of list
+ * @index: index to delete node at
+ *
+ * Return: 1 if succeded, -1 if it fails
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i;
-	listint_t *current, *subsequent;
+	unsigned int ctr = 0;
+	listint_t *temp_h, *temp_node;
 
-	if (!head || !*head)
+	if (!*head) /* EMPTY list */
 		return (-1);
-	current = *head;
+	temp_h = *head;
 	if (index == 0)
 	{
-		*head = (*head)->next;
-		free(current);
+		temp_node = temp_h->next;
+		*head = temp_node;
+		free(temp_h);
 		return (1);
 	}
-	for (i = 0; i < (index - 1); i++)
+	while (temp_h)
 	{
-		current = current->next;
-		if (current == NULL)
-			return (-1);
+		if (ctr + 1 == index) /* delete node here */
+		{
+			temp_node = temp_h->next;
+			temp_h->next = temp_node->next;
+			free(temp_node);
+			return (1);
+		}
+		temp_h = temp_h->next;
+		ctr++;
 	}
-	subsequent = current->next;
-	current->next = subsequent->next;
-	free(subsequent);
-	return (1);
+	return (-1); /* didn't find node */
 }
