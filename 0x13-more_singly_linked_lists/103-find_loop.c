1debc52ff8b79e7e219f78e93ac67d41c923b53f
Wakilat <wakilatadeola@gmail.com>
2022-04-25 12:15:46 -0700

    Singly linked lists

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,11 +1,29 @@
 #include "lists.h"
+
 /**
- * find_listint_loop - function that finds the loop in a linked list
- * @head:
- * Return: return loop
+ * find_listint_loop - Funcion dada
+ * @head: Desde main
+ *
+ * Return: slow_p
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-  head++;
-  return (0);
+	listint_t *slow_p = head, *fast_p = head;
+
+	while (slow_p && fast_p && fast_p->next)
+	{
+		slow_p = slow_p->next;
+		fast_p = fast_p->next->next;
+		if (slow_p == fast_p)
+		{
+			slow_p = head;
+			while (slow_p != fast_p)
+			{
+				slow_p = slow_p->next;
+				fast_p = fast_p->next;
+			}
+			return (slow_p);
+		}
+	}
+	return (NULL);
 }
