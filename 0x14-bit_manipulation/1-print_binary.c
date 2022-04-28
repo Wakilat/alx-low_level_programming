1d27862fa56d358e70eec20d7e0063744b6108f3
Wakilat <wakilatadeola@gmail.com>
2022-04-28 12:18:43 -0700

    bitwise

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,18 +7,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int mask = 32768; /* 1000 0000 0000 0000 */
-	char hit_rel_flag = 0;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	while (mask > 0)
-	{
-		if ((n & mask) == 0 && (hit_rel_flag || mask == 0x01))
-			_putchar('0');
-		else if ((n & mask) != 0)
-		{
-			_putchar('1');
-			hit_rel_flag = 1;
-		}
-		mask = mask >> 1;
-	}
+	_putchar((n & 1) + '0');
 }
